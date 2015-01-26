0ad1bcefa9ec3c7a05465be886e313a5546e376b
Security-Database Admin <info@security-database.com>
2015-01-26 16:40:19 +0100

    reindent lines

--- cvss.class.php
+++ cvss.class.php
@@ -46,57 +46,57 @@ Class Cvss {
 	private $vector_input_array = array();
 
 	private $metrics_check_mandatory = array("AV"=>"[N,A,L,P]",
-											"AC"=>"[L,H]",
-											"PR"=>"[N,L,H]",
-											"UI"=>"[N,R]",
-											"S"=>"[U,C]",
-											"C"=>"[N,L,H]",
-											"I"=>"[N,L,H]",
-											"A"=>"[N,L,H]"
-											);
+		"AC"=>"[L,H]",
+		"PR"=>"[N,L,H]",
+		"UI"=>"[N,R]",
+		"S"=>"[U,C]",
+		"C"=>"[N,L,H]",
+		"I"=>"[N,L,H]",
+		"A"=>"[N,L,H]"
+		);
 
 	private $metrics_check_optional = array("E"=>"[X,U,P,F,H]",
-											"RL"=>"[X,O,T,W,U]",
-											"RC"=>"[X,U,R,C]",
-											"CR"=>"[X,L,M,H]",
-											"IR"=>"[X,L,M,H]",
-											"AR"=>"[X,L,M,H]"
-											);
+		"RL"=>"[X,O,T,W,U]",
+		"RC"=>"[X,U,R,C]",
+		"CR"=>"[X,L,M,H]",
+		"IR"=>"[X,L,M,H]",
+		"AR"=>"[X,L,M,H]"
+		);
 
 	private $metrics_check_modified = array("MAV"=>"[X,N,A,L,P]",
-											"MAC"=>"[X,L,H]",
-											"MPR"=>"[X,N,L,H]",
-											"MUI"=>"[X,N,R]",
-											"MS"=>"[X,U,C]",
-											"MC"=>"[X,N,L,H]",
-											"MI"=>"[X,N,L,H]",
-											"MA"=>"[X,N,L,H]"
-											);
+		"MAC"=>"[X,L,H]",
+		"MPR"=>"[X,N,L,H]",
+		"MUI"=>"[X,N,R]",
+		"MS"=>"[X,U,C]",
+		"MC"=>"[X,N,L,H]",
+		"MI"=>"[X,N,L,H]",
+		"MA"=>"[X,N,L,H]"
+		);
 
 	private $metrics_level_mandatory = array("AV" => array("N" => '0.85', "A" => '0.62', "L" => '0.55', "P" => '0.2'),
-											"AC" => array("L" => '0.77', "H" => '0.44'),
-											"PR" => array("N" => '0.85', "L" => array("Default" => '0.62', "Scope" => '0.68'), "H" => array("Default" => '0.27', "Scope" => '0.50')),
-											"UI" => array("N" => '0.85', "R" => '0.62'),
-											"C" => array("N" => '0', "L" => '0.22', "H" => '0.56'),
-											"I" => array("N" => '0', "L" => '0.22', "H" => '0.56'),
-											"A" => array("N" => '0', "L" => '0.22', "H" => '0.56')
-											);
+		"AC" => array("L" => '0.77', "H" => '0.44'),
+		"PR" => array("N" => '0.85', "L" => array("Default" => '0.62', "Scope" => '0.68'), "H" => array("Default" => '0.27', "Scope" => '0.50')),
+		"UI" => array("N" => '0.85', "R" => '0.62'),
+		"C" => array("N" => '0', "L" => '0.22', "H" => '0.56'),
+		"I" => array("N" => '0', "L" => '0.22', "H" => '0.56'),
+		"A" => array("N" => '0', "L" => '0.22', "H" => '0.56')
+		);
 	private $metrics_level_optional = array("E" => array("X" => '1', "U" => '0.91', "P" => '0.94', "F" => '0.97', "H" => '1'),
-											"RL" => array("X" => '1', "O" => '0.95', "T" => '0.96', "W" => '0.97', "U" => '1'),
-											"RC" => array("X" => '1', "U" => '0.92', "R" => '0.96', "C" => '1'),
-											"CR" => array("X" => '1', "L" => '0.5', "M" => '1', "H" => '1.5'),
-											"IR" => array("X" => '1', "L" => '0.5', "M" => '1', "H" => '1.5'),
-											"AR" => array("X" => '1', "L" => '0.5', "M" => '1', "H" => '1.5')
-											);
+		"RL" => array("X" => '1', "O" => '0.95', "T" => '0.96', "W" => '0.97', "U" => '1'),
+		"RC" => array("X" => '1', "U" => '0.92', "R" => '0.96', "C" => '1'),
+		"CR" => array("X" => '1', "L" => '0.5', "M" => '1', "H" => '1.5'),
+		"IR" => array("X" => '1', "L" => '0.5', "M" => '1', "H" => '1.5'),
+		"AR" => array("X" => '1', "L" => '0.5', "M" => '1', "H" => '1.5')
+		);
 
 	private $metrics_level_modified = array("MAV" => array("X"=>'0', "N" => '0.85', "A" => '0.62', "L" => '0.55', "P" => '0.2'),
-								   			"MAC" => array("X"=>'0', "L" => '0.77', "H" => '0.44'),
-								   			"MPR" => array("X"=>'0',"N" => '0.85', "L" => array("Default" => '0.62', "Scope" => '0.68'), "H" => array("Default" => '0.27', "Scope" => '0.50')),
-								   			"MUI" => array("X"=>'0',"N" => '0.85', "R" => '0.62'),	
-								   			"MC" => array("X"=>'0',"N" => '0', "L" => '0.22', "H" => '0.56'),
-								   			"MI" => array("X"=>'0',"N" => '0', "L" => '0.22', "H" => '0.56'),
-								   			"MA" => array("X"=>'0',"N" => '0', "L" => '0.22', "H" => '0.56')
-								   			);
+		"MAC" => array("X"=>'0', "L" => '0.77', "H" => '0.44'),
+		"MPR" => array("X"=>'0',"N" => '0.85', "L" => array("Default" => '0.62', "Scope" => '0.68'), "H" => array("Default" => '0.27', "Scope" => '0.50')),
+		"MUI" => array("X"=>'0',"N" => '0.85', "R" => '0.62'),	
+		"MC" => array("X"=>'0',"N" => '0', "L" => '0.22', "H" => '0.56'),
+		"MI" => array("X"=>'0',"N" => '0', "L" => '0.22', "H" => '0.56'),
+		"MA" => array("X"=>'0',"N" => '0', "L" => '0.22', "H" => '0.56')
+		);
 
 	public function register($vector) {
 		self::_explode_vector($vector);
@@ -109,16 +109,16 @@ Class Cvss {
 	}
 
 	private function _explode_vector($vector) {
-			$vector = str_replace("CVSS:3.0/", "", $vector);
-			$vector_input_array_temp = array();
-			$vector_input_array = array();
-			$vector_input_array_temp = explode('/', $vector);
+		$vector = str_replace("CVSS:3.0/", "", $vector);
+		$vector_input_array_temp = array();
+		$vector_input_array = array();
+		$vector_input_array_temp = explode('/', $vector);
 
-			foreach($vector_input_array_temp as $k => $v) {
-				$temp = array();
-				$temp = explode(":", $v);
-				$this->vector_input_array[$temp[0]] = $temp[1];
-			}
+		foreach($vector_input_array_temp as $k => $v) {
+			$temp = array();
+			$temp = explode(":", $v);
+			$this->vector_input_array[$temp[0]] = $temp[1];
+		}
 	}
 
 
@@ -239,95 +239,95 @@ Class Cvss {
 		/* 	ISC : Impact Sub score 
 			ESC : Exploitability Sub score
 		*/ 
-	
-		/* Impact Sub base score  */
+			
+			/* Impact Sub base score  */
 
-		$this->calcul["ISCbase"] = 1 - ( ( 1 - $this->scores["C"] ) * ( 1 - $this->scores["I"] ) * ( 1 - $this->scores["A"] ) );
-		$this->formula["ISCbase"] = "1 - ( ( 1 - ".$this->scores["C"]." ) * ( 1 - ".$this->scores["I"]." ) * ( 1 - ".$this->scores["A"]." ) )";
-		/* Impact Sub Score */
-		if ($this->vector_input_array["S"] == 'U') {
-			$this->calcul["ISC"] = 6.42 * $this->calcul["ISCbase"];
-			$this->formula["ISC"] = "6.42 * ".$this->calcul["ISCbase"];
-		} elseif ($this->vector_input_array["S"] == 'C') {
-			$this->calcul["ISC"] = 7.52 * ( $this->calcul["ISCbase"] - 0.029 ) - 3.25 * pow(( $this->calcul["ISCbase"] - 0.02 ),15);
-			$this->formula["ISC"] = "7.52 * ( ".$this->calcul["ISCbase"]." - 0.029 ) - 3.25 * pow(( ".$this->calcul["ISCbase"]." - 0.02 ),15)";
-		} else {
-			throw new Exception("ERROR: on Scope", __LINE__);
-			return;
+			$this->calcul["ISCbase"] = 1 - ( ( 1 - $this->scores["C"] ) * ( 1 - $this->scores["I"] ) * ( 1 - $this->scores["A"] ) );
+			$this->formula["ISCbase"] = "1 - ( ( 1 - ".$this->scores["C"]." ) * ( 1 - ".$this->scores["I"]." ) * ( 1 - ".$this->scores["A"]." ) )";
+			/* Impact Sub Score */
+			if ($this->vector_input_array["S"] == 'U') {
+				$this->calcul["ISC"] = 6.42 * $this->calcul["ISCbase"];
+				$this->formula["ISC"] = "6.42 * ".$this->calcul["ISCbase"];
+			} elseif ($this->vector_input_array["S"] == 'C') {
+				$this->calcul["ISC"] = 7.52 * ( $this->calcul["ISCbase"] - 0.029 ) - 3.25 * pow(( $this->calcul["ISCbase"] - 0.02 ),15);
+				$this->formula["ISC"] = "7.52 * ( ".$this->calcul["ISCbase"]." - 0.029 ) - 3.25 * pow(( ".$this->calcul["ISCbase"]." - 0.02 ),15)";
+			} else {
+				throw new Exception("ERROR: on Scope", __LINE__);
+				return;
+			}
+
+			/* Exploitability Sub score */
+			$this->calcul["ESC"] = 8.22 * $this->scores["AV"] * $this->scores["AC"] * $this->scores["PR"] * $this->scores["UI"];
+			$this->formula["ESC"] = "8.22 * ".$this->scores["AV"]." * ".$this->scores["AC"]." * ".$this->scores["PR"]." * ".$this->scores["UI"];
+
+			/* Base Score */
+			if ($this->calcul["ISC"] <= 0 ) {
+				$this->calcul["BS"] = 0;
+				$this->formula["BS"] = "0";
+			} elseif ( $this->calcul["ISC"] > 0 && $this->vector_input_array["S"] == 'U' ) {
+				$this->calcul["BS"] =  self::round_up( min(10, $this->calcul["ISC"] + $this->calcul["ESC"]) , 1);
+				$this->formula["BS"] = "round_up( min( 10 , ".$this->calcul["ISC"]." + ".$this->calcul["ESC"]." ) )";
+			} elseif ( $this->calcul["ISC"] > 0 && $this->vector_input_array["S"] == 'C' ) {
+				$this->calcul["BS"] =  self::round_up( min(10, 1.08 * ($this->calcul["ISC"] + $this->calcul["ESC"])) , 1);
+				$this->formula["BS"] = "round_up( min( 10 , 1.08 * ( ".$this->calcul["ISC"]." + ".$this->calcul["ESC"]." ) ) )";
+
+			} else {
+				throw new Exception("ERROR: on Base Score calcul", __LINE__);
+				return;
+			}
+
+			/* Temporal score */
+			$this->calcul["TS"] =  self::round_up($this->calcul["BS"] * $this->scores["E"] * $this->scores["RL"] * $this->scores["RC"],1);
+			$this->formula["TS"] =  "round_up( ".$this->calcul["BS"]." * ".$this->scores["E"]." * ".$this->scores["RL"]." * ".$this->scores["RC"].")";
+
+			/* Environmental score */
+
+			/* Modified Exploitability Sub score */
+
+			$this->calcul["MESC"] = 8.22 * $this->scores["MAV"] * $this->scores["MAC"] * $this->scores["MPR"] * $this->scores["MUI"];
+			$this->formula["MESC"] = "8.22 * ".$this->scores["MAV"]." * ".$this->scores["MAC"]." * ".$this->scores["MPR"]." * ".$this->scores["MUI"];
+
+			/* Modified Impact Sub score */
+			$this->calcul["ISCmodified"] = min(0.915, 1 - ( ( 1 - $this->scores["MC"] * $this->scores["CR"]) * ( 1 - $this->scores["MI"] * $this->scores["IR"] ) * ( 1 - $this->scores["MA"] * $this->scores["AR"]) ) );
+			$this->formula["ISCmodified"] = "min( 0.915, 1 - ( ( 1 - ".$this->scores["MC"]." * ".$this->scores["CR"]." ) * ( 1 - ".$this->scores["MI"]." * ".$this->scores["IR"]." ) * ( 1 - ".$this->scores["MA"]." * ".$this->scores["AR"]." ) ) )";
+
+			if ($this->vector_input_array["S"] == 'U') {
+				$this->calcul["MISS"] = 6.42 * $this->calcul["ISCmodified"];
+				$this->formula["MISS"] = "6.42 * ".$this->calcul["ISCmodified"];
+			} elseif ($this->vector_input_array["S"] == 'C') {
+				$this->calcul["MISS"] = 7.52 * ( $this->calcul["ISCmodified"] - 0.029 ) - 3.25 * pow(( $this->calcul["ISCmodified"] - 0.02 ),15);
+				$this->formula["MISS"] = "7.52 * ( ".$this->calcul["ISCmodified"]." - 0.029 ) - 3.25 * pow(( ".$this->calcul["ISCmodified"]." - 0.02 ),15)";
+			} else {
+				throw new Exception("ERROR: on Modified Impact Sub score calcul", __LINE__);
+				return;
+			}
+
+			/* Environmental Score */
+			if ($this->calcul["MISS"] <= 0 ) {
+				$this->calcul["ES"] = 0;
+				$this->formula["ES"] = "0";
+			} elseif ( $this->calcul["MISS"] > 0 && $this->vector_input_array["S"] == 'U' ) {
+				$this->calcul["ES"] = self::round_up(min(10, ($this->calcul["MISS"] + $this->calcul["MESC"] ) * $this->scores["E"] * $this->scores["RL"] * $this->scores["RC"]),1);
+				$this->formula["ES"] = "round_up(min(10 , (".$this->calcul["MISS"]." + ".$this->calcul["MESC"]." ) * ".$this->scores["E"]." * ".$this->scores["RL"]." * ".$this->scores["RC"]."),1)";
+			} elseif ( $this->calcul["MISS"] > 0 && $this->vector_input_array["S"] == 'C' ) {
+				$this->calcul["ES"] = self::round_up(min(10, 1.08 * ($this->calcul["MISS"] + $this->calcul["MESC"] ) * $this->scores["E"] * $this->scores["RL"] * $this->scores["RC"]),1);
+				$this->formula["ES"] = "round_up(min(10 , 1.08 * (".$this->calcul["MISS"]." + ".$this->calcul["MESC"]." ) * ".$this->scores["E"]." * ".$this->scores["RL"]." * ".$this->scores["RC"]."),1)";
+			} else {
+				throw new Exception("ERROR: on Environmental Score calcul", __LINE__);
+				return;
+			}
 		}
 
-		/* Exploitability Sub score */
-		$this->calcul["ESC"] = 8.22 * $this->scores["AV"] * $this->scores["AC"] * $this->scores["PR"] * $this->scores["UI"];
-		$this->formula["ESC"] = "8.22 * ".$this->scores["AV"]." * ".$this->scores["AC"]." * ".$this->scores["PR"]." * ".$this->scores["UI"];
-
-		/* Base Score */
-		if ($this->calcul["ISC"] <= 0 ) {
-			$this->calcul["BS"] = 0;
-			$this->formula["BS"] = "0";
-		} elseif ( $this->calcul["ISC"] > 0 && $this->vector_input_array["S"] == 'U' ) {
-			$this->calcul["BS"] =  self::round_up( min(10, $this->calcul["ISC"] + $this->calcul["ESC"]) , 1);
-			$this->formula["BS"] = "round_up( min( 10 , ".$this->calcul["ISC"]." + ".$this->calcul["ESC"]." ) )";
-		} elseif ( $this->calcul["ISC"] > 0 && $this->vector_input_array["S"] == 'C' ) {
-			$this->calcul["BS"] =  self::round_up( min(10, 1.08 * ($this->calcul["ISC"] + $this->calcul["ESC"])) , 1);
-			$this->formula["BS"] = "round_up( min( 10 , 1.08 * ( ".$this->calcul["ISC"]." + ".$this->calcul["ESC"]." ) ) )";
-
-		} else {
-			throw new Exception("ERROR: on Base Score calcul", __LINE__);
-			return;
+		private function _construct_vector() {
+			$this->vector = "CVSS:3.0";
+			foreach ($this->vector_input_array as $vec => $input) {
+				$this->vector .= "/".$vec.":".$input;
+			}
 		}
 
-		/* Temporal score */
-		$this->calcul["TS"] =  self::round_up($this->calcul["BS"] * $this->scores["E"] * $this->scores["RL"] * $this->scores["RC"],1);
-		$this->formula["TS"] =  "round_up( ".$this->calcul["BS"]." * ".$this->scores["E"]." * ".$this->scores["RL"]." * ".$this->scores["RC"].")";
-
-		/* Environmental score */
-
-		/* Modified Exploitability Sub score */
-
-		$this->calcul["MESC"] = 8.22 * $this->scores["MAV"] * $this->scores["MAC"] * $this->scores["MPR"] * $this->scores["MUI"];
-		$this->formula["MESC"] = "8.22 * ".$this->scores["MAV"]." * ".$this->scores["MAC"]." * ".$this->scores["MPR"]." * ".$this->scores["MUI"];
-
-		/* Modified Impact Sub score */
-		$this->calcul["ISCmodified"] = min(0.915, 1 - ( ( 1 - $this->scores["MC"] * $this->scores["CR"]) * ( 1 - $this->scores["MI"] * $this->scores["IR"] ) * ( 1 - $this->scores["MA"] * $this->scores["AR"]) ) );
-		$this->formula["ISCmodified"] = "min( 0.915, 1 - ( ( 1 - ".$this->scores["MC"]." * ".$this->scores["CR"]." ) * ( 1 - ".$this->scores["MI"]." * ".$this->scores["IR"]." ) * ( 1 - ".$this->scores["MA"]." * ".$this->scores["AR"]." ) ) )";
-
-		if ($this->vector_input_array["S"] == 'U') {
-			$this->calcul["MISS"] = 6.42 * $this->calcul["ISCmodified"];
-			$this->formula["MISS"] = "6.42 * ".$this->calcul["ISCmodified"];
-		} elseif ($this->vector_input_array["S"] == 'C') {
-			$this->calcul["MISS"] = 7.52 * ( $this->calcul["ISCmodified"] - 0.029 ) - 3.25 * pow(( $this->calcul["ISCmodified"] - 0.02 ),15);
-			$this->formula["MISS"] = "7.52 * ( ".$this->calcul["ISCmodified"]." - 0.029 ) - 3.25 * pow(( ".$this->calcul["ISCmodified"]." - 0.02 ),15)";
-		} else {
-			throw new Exception("ERROR: on Modified Impact Sub score calcul", __LINE__);
-			return;
-		}
-
-		/* Environmental Score */
-		if ($this->calcul["MISS"] <= 0 ) {
-			$this->calcul["ES"] = 0;
-			$this->formula["ES"] = "0";
-		} elseif ( $this->calcul["MISS"] > 0 && $this->vector_input_array["S"] == 'U' ) {
-			$this->calcul["ES"] = self::round_up(min(10, ($this->calcul["MISS"] + $this->calcul["MESC"] ) * $this->scores["E"] * $this->scores["RL"] * $this->scores["RC"]),1);
-			$this->formula["ES"] = "round_up(min(10 , (".$this->calcul["MISS"]." + ".$this->calcul["MESC"]." ) * ".$this->scores["E"]." * ".$this->scores["RL"]." * ".$this->scores["RC"]."),1)";
-		} elseif ( $this->calcul["MISS"] > 0 && $this->vector_input_array["S"] == 'C' ) {
-			$this->calcul["ES"] = self::round_up(min(10, 1.08 * ($this->calcul["MISS"] + $this->calcul["MESC"] ) * $this->scores["E"] * $this->scores["RL"] * $this->scores["RC"]),1);
-			$this->formula["ES"] = "round_up(min(10 , 1.08 * (".$this->calcul["MISS"]." + ".$this->calcul["MESC"]." ) * ".$this->scores["E"]." * ".$this->scores["RL"]." * ".$this->scores["RC"]."),1)";
-		} else {
-			throw new Exception("ERROR: on Environmental Score calcul", __LINE__);
-			return;
+		/* Tools */
+		public function round_up ( $value, $precision ) { 
+			$pow = pow ( 10, $precision ); 
+			return ( ceil ( $pow * $value ) + ceil ( $pow * $value - ceil ( $pow * $value ) ) ) / $pow; 
 		}
 	}
-
-	private function _construct_vector() {
-		$this->vector = "CVSS:3.0";
-		foreach ($this->vector_input_array as $vec => $input) {
-			$this->vector .= "/".$vec.":".$input;
-		}
-	}
-
-	/* Tools */
-	public function round_up ( $value, $precision ) { 
-    	$pow = pow ( 10, $precision ); 
-    	return ( ceil ( $pow * $value ) + ceil ( $pow * $value - ceil ( $pow * $value ) ) ) / $pow; 
-    }
-}
